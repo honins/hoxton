cb8d7b4e7d7376080cffa5b52d58bdafe65201d2
honins <honins@qq.com>
2020-04-02 17:55:04 +0800

    Initial commit

---
+++ src/main/java/com/example/spring/cloud/hoxton/HoxtonApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.cloud.hoxton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoxtonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoxtonApplication.class, args);
+    }
+
+}
